44d6cdc4f71e903928b62f12fcd29d3a100543ae
Najib Ishaq <najib_ishaq@uri.edu>
2021-04-25 00:12:56 -0400

    got data

--- model/utils.py
+++ model/utils.py
@@ -6,23 +6,14 @@ MODELS_DIR = os.path.join(ROOT_DIR, 'saved_models')
 RESULTS_DIR = os.path.join(ROOT_DIR, 'results')
 
 DATA_DIR = '/data/kaggle/hubmap'
+TRAIN_DIR = os.path.join(DATA_DIR, 'train')
+TEST_DIR = os.path.join(DATA_DIR, 'test')
 TRAIN_PATH = os.path.join(DATA_DIR, 'train.csv')
-FEATURES_PATH = os.path.join(DATA_DIR, 'features.csv')
-EXAMPLE_TEST_PATH = os.path.join(DATA_DIR, 'example_test.csv')
-EXAMPLE_SUBMISSION_PATH = os.path.join(DATA_DIR, 'example_sample_submission.csv')
-
-TRAIN_X_PATH = os.path.join(DATA_DIR, 'train_x.npy')
-TRAIN_Y_PATH = os.path.join(DATA_DIR, 'train_y.npy')
-
-VAL_X_PATH = os.path.join(DATA_DIR, 'val_x.npy')
-VAL_Y_PATH = os.path.join(DATA_DIR, 'val_y.npy')
-
-TEST_X_PATH = os.path.join(DATA_DIR, 'test_x.npy')
-TEST_Y_PATH = os.path.join(DATA_DIR, 'test_y.npy')
+SAMPLE_SUBMISSION_PATH = os.path.join(DATA_DIR, 'sample_submission.csv')
 
 
 if __name__ == '__main__':
-    for _path in [TRAIN_PATH, FEATURES_PATH, EXAMPLE_TEST_PATH, EXAMPLE_SUBMISSION_PATH]:
+    for _path in [TRAIN_PATH, SAMPLE_SUBMISSION_PATH]:
         assert os.path.exists(_path)
 
     for _dir in [LOGS_DIR, MODELS_DIR, RESULTS_DIR]:
