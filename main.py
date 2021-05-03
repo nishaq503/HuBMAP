a9e0f47954d4a90214a3bb9db29259c88e884b66
Najib Ishaq <najib_ishaq@uri.edu>
2021-05-03 07:31:18 -0400

    proper fold selection

--- main.py
+++ main.py
@@ -8,25 +8,10 @@ from datagen import TrainSequence
 from net import HubmapMasker
 
 
-def train_model(
-        fold: int,
-        initial_epoch: int,
-        final_epoch: int,
-        file_ids: List[str],
-):
-    if fold >= 4:
-        raise ValueError(f'We only do 4 folds. Fold must be one of [0, 1, 2, 3]. Got {fold} instead.')
-
-    keras.backend.clear_session()
-
-    val_start, val_end = fold * 3, (fold + 1) * 3
-    train_ids = file_ids[:val_start] + file_ids[val_end:]
-    val_ids = file_ids[val_start:val_end]
-
-    model_name = f'main_fold_{fold}'
+def train_model(model_name: str, initial_epoch: int, final_epoch: int, train_ids: List[str], val_ids: List[str]):
     if initial_epoch == 0:
         filters = [
-            utils.GLOBALS['base_filters'] * (1 + i)
+            utils.GLOBALS['base_filters'] * (2 ** i)
             for i in range(utils.GLOBALS['model_depth'])
         ]
         model = HubmapMasker(
@@ -44,8 +29,8 @@ def train_model(
     # exit(1)
 
     weights = {
-        'embedding': 4,
-        'autoencoder': 4,
+        'embedding': 1 / 256,
+        'autoencoder': 1,
         'mask': 1,
     }
     model.compile(weights=weights)
@@ -65,17 +50,41 @@ def train_model(
     return
 
 
-if __name__ == '__main__':
-    # utils.delete_old()
+def train_fold(model_name: str, fold: int, fold_size: int, file_ids: List[str]):
+    if len(file_ids) % fold_size != 0:
+        raise ValueError(f'fold_size {fold_size} with {len(file_ids)} files results in uneven fold sizes.')
 
-    _model_name = 'main_model'
+    if not 0 <= fold < len(file_ids) // fold_size:
+        raise IndexError(f'fold {fold} is out of index at a fold_size of {fold_size} with {len(file_ids)} files.'
+                         f'fold must be between 0 and {len(file_ids) // fold_size - 1}.')
+
+    keras.backend.clear_session()
+
+    val_start, val_end = fold * fold_size, (fold + 1) * fold_size
+    train_ids = file_ids[:val_start] + file_ids[val_end:]
+    val_ids = file_ids[val_start:val_end]
+
+    model_name = f'{model_name}_{fold}'
+    train_model(
+        model_name=model_name,
+        initial_epoch=0,
+        final_epoch=5,
+        train_ids=train_ids,
+        val_ids=val_ids,
+    )
+    return
+
+
+if __name__ == '__main__':
+    utils.delete_old()
+
     _file_ids = [_name.split('/')[-1].split('.')[0] for _name in glob(f'{utils.TRAIN_DIR}/*.tiff')]
     _file_ids = list(sorted(_file_ids))
-    _train_ids = _file_ids[:12]
     _test_ids = _file_ids[12:]
-    train_model(
+    _file_ids = _file_ids[:12]
+    train_fold(
+        model_name='test_model',
         fold=0,
-        initial_epoch=0,
-        final_epoch=1,
-        file_ids=_train_ids,
+        fold_size=3,
+        file_ids=_file_ids,
     )
